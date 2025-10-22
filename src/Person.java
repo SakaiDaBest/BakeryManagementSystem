50653ba9067b1b5b3cc21089646a9cbbbd6fa602
Wong Wei Jun <wong.weijun923@gmail.com>
2025-10-22 22:01:40 +0800

    Added csv functions

--- src/Person.java
+++ src/Person.java
@@ -2,50 +2,95 @@ import java.io.*;
 import java.util.*;
 
 public class Person {
-    String name;
-    String dateOfBirth;
-    String id;
-    String phoneNumber;
+    protected String name;
+    protected String dateOfBirth;
+    protected String id;
+    protected String phoneNumber;
 
-    public Person(String name, String dateOfBirth, String phoneNumber){
+    public Person(String name, String dateOfBirth, String phoneNumber) {
         this.dateOfBirth = dateOfBirth;
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
 
-}
+    // --- Shared ID generation logic ---
+    protected static String generateNewId(String fileName, String prefix) {
+        String lastId = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lastId = line.split(",")[0]; // first column = ID
+            }
+        } catch (FileNotFoundException e) {
+            // file might not exist yet, start fresh
+            System.out.println("No existing file found for " + prefix + ". Starting at " + prefix + "001");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-class Customer extends Person{
-
-    private static final String FILE = "customers.csv";
-
-    public Customer(String name, String dateOfBirth, String id, String phoneNumber){
-        super(name, dateOfBirth, phoneNumber);
-        this.id = id;
+        if (lastId == null) {
+            return prefix + "001";
+        } else {
+            int num = Integer.parseInt(lastId.substring(1));
+            num++;
+            return String.format("%s%03d", prefix, num);
+        }
     }
 
-    public static void addCustomer(String name, String dateOfBirth, String id, String phoneNumber){
-        try(FileWriter fw = new FileWriter(FILE, true)){
-            fw.write(id + "," + name + "," + dateOfBirth + "," + phoneNumber);
-        } catch(IOException e){
+    // --- Shared save logic ---
+    protected static void saveToFile(String fileName, String record) {
+        try (FileWriter fw = new FileWriter(fileName, true)) {
+            fw.write(record + "\n");
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    // --- Shared read logic ---
+    protected static void readFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("No records found in " + fileName);
         }
     }
 }
 
-class Manager extends Person{
-    private static final String FILE = "managers.csv";
+class Customer extends Person {
+    private static final String FILE = "src/customers.csv";
 
-    public Manager(String name, String dateOfBirth, String id, String phoneNumber){
+    public Customer(String name, String dateOfBirth, String phoneNumber) {
         super(name, dateOfBirth, phoneNumber);
-        this.id = id;
+        this.id = generateNewId(FILE, "C");
     }
 
-    public static void addManager(String name, String dateOfBirth, String id, String phoneNumber){
-        try(FileWriter fw = new FileWriter(FILE, true)){
-            fw.write(id + "," + name + "," + dateOfBirth + "," + phoneNumber);
-        } catch(IOException e){
-            e.printStackTrace();
-        }
+    public void save() {
+        saveToFile(FILE, id + "," + name + "," + dateOfBirth + "," + phoneNumber);
+    }
+
+    public static void readCustomers() {
+        System.out.println("📋 Customer List:");
+        readFile(FILE);
+    }
+}
+
+class Manager extends Person {
+    private static final String FILE = "src/managers.csv";
+
+    public Manager(String name, String dateOfBirth, String phoneNumber) {
+        super(name, dateOfBirth, phoneNumber);
+        this.id = generateNewId(FILE, "M");
+    }
+
+    public void save() {
+        saveToFile(FILE, id + "," + name + "," + dateOfBirth + "," + phoneNumber);
+    }
+
+    public static void readManagers() {
+        System.out.println("👔 Manager List:");
+        readFile(FILE);
     }
 }
