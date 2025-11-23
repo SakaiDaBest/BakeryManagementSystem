1ef5dcf6ab5d2b06d80f4691fba998319a3f31e5
Wong Wei Jun <162281012+SakaiDaBest@users.noreply.github.com>
2025-11-23 18:35:08 +0800

    Update BMS_FinalTech.java

--- src/BMS_FinalTech.java
+++ src/BMS_FinalTech.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 import java.io.*;
-import java.nio.Buffer;
 import java.util.*;
 
 public class BMS_FinalTech {
     static final String FILE_CUSTOMER = System.getProperty("user.dir") + "/BakeryManagementSystem/src/customers.csv";;
     static final String FILE_MANAGER = System.getProperty("user.dir") + "/BakeryManagementSystem/src/managers.csv";;
-    static final String MASTER_PASSWORD = "12345";
+    /* GeeksforGeeks. (2022, January 21). getproperty() and getproperties() methods of System Class in Java.
+    from https://www.geeksforgeeks.org/java/getproperty-and-getproperties-methods-of-system-class-in-java/*/
+
+    static final String MASTER_PASSWORD = "12345"; //used for creating new manager
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
         System.out.println("=====================================");
-        System.out.println("     🍞 Bakery Management System");
+        System.out.println("       Bakery Management System");
         System.out.println("=====================================");
 
         while (running) {
@@ -27,44 +30,37 @@ public class BMS_FinalTech {
             char state = 0;
             switch (choice) {
                 case "1":
-                    String[] userINFO=null;
+                    String[] userINFO = null;
                     boolean exit = false;
-                    while(!loggedIn){
+                    while(!loggedIn && !exit){
                         System.out.print("\nEnter your User ID (or X to quit): ");
                         String userId = scanner.nextLine().trim();
 
                         if (userId.isEmpty()) {
-                            System.out.println("⚠️  User ID cannot be empty.");
-                        } else {
-                            if (userId.charAt(0) == 'M') {
-                                state = 'M';
-                                userINFO = authentication(FILE_MANAGER, userId);
+                            System.out.println("User ID cannot be empty.");
+                            continue;
+                        }
 
-                            }else if(userId.charAt(0)=='C'){
-                                state = 'C';
-                                userINFO = authentication(FILE_CUSTOMER, userId);
-                            }else if(userId.charAt(0)=='X'){
-                                exit = true;
-                                break;
-                            }else{
-                                break;
-                            }
-                            if (userINFO.length == 4) {
-                                loggedIn = true;
-                            }
+                        if (userId.charAt(0) == 'M') {//verify manager info
+                            state = 'M';
+                            userINFO = authentication(FILE_MANAGER, userId);
+                        } else if(userId.charAt(0) == 'C'){//verify customer info
+                            state = 'C';
+                            userINFO = authentication(FILE_CUSTOMER, userId);
+                        } else if(userId.charAt(0) == 'X' || userId.charAt(0) == 'x'){
+                            exit = true;
+                            break;
+                        } else {
+                            System.out.println("Invalid User ID format.");
+                            continue;
+                        }
+
+                        if (userINFO.length == 4) {
+                            loggedIn = true;
+                            Person.showUI(state, userINFO);  //show UI after successful login
                         }
                     }
-
-                    if(exit){
-                        break;
-                    }
-                    if(state!=0){
-                        Person.showUI(state, userINFO);
-                    }
-
-
                     break;
-
                 case "2":
                     System.out.print("\nEnter your Name: ");
                     String cname = scanner.next();
@@ -73,35 +69,39 @@ public class BMS_FinalTech {
                     System.out.print("\nEnter your Phone Number: ");
                     String cphoneNumber = scanner.next();
                     Customer customer = new Customer(cname, cdate, cphoneNumber);
-                    customer.save();
-                    //auto login
+                    customer.save(); //save new customer to customer.csv
+                    state='C';
+                    Person.showUI(state, customer.getInfo()); //shows customer UI after login
+                    break;
 
                 case "3":
                     System.out.print("\nEnter your Master Password: ");
-                    String mpass = scanner.next();
-                    if(mpass != MASTER_PASSWORD){
+                    String mpass = scanner.nextLine();
+                    if(!mpass.equals(MASTER_PASSWORD)){
                         System.out.println("Master Password is Incorrect");
                         break;
                     }
                     System.out.print("\nEnter your Name: ");
-                    String mname = scanner.next();
+                    String mname = scanner.nextLine();
                     System.out.print("\nEnter your Date of Birth(DD/MM/YYYY): ");
-                    String mdate = scanner.next();
+                    String mdate = scanner.nextLine();
                     System.out.print("\nEnter your Phone Number: ");
-                    String mphoneNumber = scanner.next();
+                    String mphoneNumber = scanner.nextLine();
                     Manager manager = new Manager(mname, mdate, mphoneNumber);
-                    manager.save();
-                    //auto login
+                    manager.save(); //save new customer to manager.csv
+                    state='M';
+                    Person.showUI(state, manager.getInfo()); //shows manager UI after login
 
+                    break;
 
 
                 case "4":
-                    System.out.println("\n👋 Exiting Bakery Management System...");
+                    System.out.println("\nExiting Bakery Management System...");
                     running = false;
                     break;
 
                 default:
-                    System.out.println("❌ Invalid choice. Please enter 1, 2, 3 or 4.");
+                    System.out.println("Invalid choice. Please enter 1, 2, 3 or 4.");
                     break;
             }
         }
@@ -111,21 +111,16 @@ public class BMS_FinalTech {
     }
 
     protected static String[] authentication(String fileName, String userId){
-        String pnInput = "";
-
-        try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("\nEnter your phone number: ");
-            pnInput = br.readLine();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+        System.out.print("\nEnter your phone number: ");
+        String pnInput = Person.scanner.nextLine().trim();
 
         String myId = null;
         String myPhone = null;
         String myName = null;
         String myDOB = null;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+//            GeeksforGeeks. (2025, July 23). Difference Between BufferedReader and FileReader in Java.
+//            from https://www.geeksforgeeks.org/java/difference-between-bufferedreader-and-filereader-in-java/
             String line;
             while ((line = reader.readLine()) != null) {
                 myId = line.split(",")[0];
@@ -142,7 +137,6 @@ public class BMS_FinalTech {
             return new String[0];
 
         } catch (IOException e) {
-            e.printStackTrace();
             return new String[0];
         }
     }
